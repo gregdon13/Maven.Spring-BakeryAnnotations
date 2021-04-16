36c83acf46896d9b9b944b7b8419165f6cbf14bf
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-04-15 20:29:17 -0400

    muffin service updated

--- src/main/java/com/zipcodewilmington/bakery/services/MuffinService.java
+++ src/main/java/com/zipcodewilmington/bakery/services/MuffinService.java
@@ -2,32 +2,41 @@ package com.zipcodewilmington.bakery.services;
 
 import com.zipcodewilmington.bakery.models.Muffin;
 import com.zipcodewilmington.bakery.repositories.MuffinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class MuffinService {
+    @Autowired
     private MuffinRepository repository;
 
     public MuffinService(MuffinRepository repository) {
         this.repository = repository;
     }
 
+    @GetMapping("/muffin")
     public Iterable<Muffin> index() {
         return repository.findAll();
     }
 
-    public Muffin show(Long id) {
+    @GetMapping("/muffin/{id}")
+    public Muffin show(@PathVariable Long id) {
         return repository.findById(id).get();
     }
 
+    @PostMapping("/muffin")
     public Muffin create(Muffin baker) {
         return repository.save(baker);
     }
 
+    @PutMapping("/muffin/{id}")
     public Muffin update(Long id, Muffin newMuffinData) {
         Muffin originalMuffin = repository.findById(id).get();
         originalMuffin.setFlavor(newMuffinData.getFlavor());
         return repository.save(originalMuffin);
     }
 
+    @DeleteMapping("/muffin/{id}")
     public Boolean delete(Long id) {
         repository.deleteById(id);
         return true;
